ba91dd354c1138a3cf119c0f14ed2a0697b1b0be
da crands <davecrands@gmail.com>
2019-03-24 11:45:08 -0400

    Greater scrollY before scroll up btn appears

--- src/components/scrollToTop.js
+++ src/components/scrollToTop.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react'
 class ScrollToTop extends Component {
     componentDidMount() {
         window.addEventListener('scroll', () => {            
-            window.scrollY > 100
+            window.scrollY > 300
             ? this.refs.scrollBtn.style.display = 'block'
             : this.refs.scrollBtn.style.display = 'none'            
         })        
     }
     scrollTop = () => {
-        console.log(window.scrollY)
-        // document.body.scrollTop = document.documentElement.scrollTop = 0;
         window.scrollTo({top:0, behavior:"smooth"})
     }
 
