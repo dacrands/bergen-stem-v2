0b70dc8411920f198dafcf3a59b0e0843e3a427a
da crands <davecrands@gmail.com>
2019-03-24 11:44:05 -0400

    Updated link URLs

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,15 +6,14 @@ const Landing = () => (
 <main className="main">
     <div className="main__info">
         <Logo title="Bergen STEM" style={{ height: '100%', width: '100%', maxHeight: '200px', marginBottom: "2rem"}}/>
-        {/* <h1>Bergen STEM</h1> */}
-        {/* <h3>Science, Technology, Engineering, Mathematics</h3> */}
     </div>
     <div className="main__side main__side--small">
         <ul>
             <li><Link to="about">About</Link></li>
             <li><a href="#contact">Join STEM</a></li>
             <li><a href="http://">3SP Program</a></li>
-            <li><a href="http://">Running Start Program</a></li>            
+            <li><a rel="noopener" target="_blank" href="https://bergen.edu/wp-content/uploads/Running-Start-Program-Poster-With-Link-1.pdf">
+            Running Start Program</a></li>            
         </ul>
     </div>
 </main>
