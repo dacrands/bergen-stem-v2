e6f81fdc877c6fc05fb2399e6ba33d9c272edf8e
da crands <davecrands@gmail.com>
2019-03-24 11:42:49 -0400

    Migrated content to card class

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,56 +11,57 @@ import ImageThree from '../components/imageThree'
 const SecondPage = () => (
   <Layout>
     <div className="container">
+
       <header className="header">
         <h1>About</h1>
         <h3>Why study STEM at BCC?</h3>
       </header>
-      <div className="about">                    
-        <div className="about__content">
-            <h3>Hands-on Faculty</h3>
-            <p>
-              At Bergen Community College, our STEM faculty understand
-              the value of proper research experience in a student's academic career. 
-              Our program provides students the guidance and support they need to answer the scientific questions that interest them most. 
-            </p>    
-        </div>        
-        <div className="about__img">
-          <Image />
-        </div>        
-      </div>    
-
-      <div className="about">                    
-      <div className="about__img">
-          <ImageTwo />
-        </div>  
-        <div className="about__content">
-            <h3>Real World Research Experience</h3>
-            <p>Thanks to the STEMatics grant, Bergen Community College is 
-              equipped with top of the line research equipment and facilities. 
-              This enables students to build skills in current technologies being used 
-              in modern industry.</p>          
-        </div>                
-      </div>    
-
-      <div className="about">                    
       
-        <div className="about__content">
-            <h3>Supportive Environment</h3>
-            <p>
-              Majoring in STEM is a difficult task, but that doesn't mean it can't be fun too.
-              At Bergen Community College, students have the opportunity to collaborate with
-              other students and faculty that share similar passions. 
-              </p>          
-        </div>       
-        <div className="about__img">
+      <div className="card">
+        <div className="card__img">
+          <Image />
+        </div>
+        <div className="card__content">
+          <h3>Hands-on Faculty</h3>
+          <p>
+            At Bergen Community College, our STEM faculty understand
+            the value of proper research experience in a student's academic career.
+            Our program provides students the guidance and support they need to answer the scientific questions that interest them most.
+            </p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__img">
+          <ImageTwo />
+        </div>
+        <div className="card__content">
+          <h3>Real World Research Experience</h3>
+          <p>Thanks to the STEMatics grant, Bergen Community College is
+            equipped with top of the line research equipment and facilities.
+            This enables students to build skills in current technologies being used
+              in modern industry.</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__img">
           <ImageThree />
-        </div>           
-      </div>    
+        </div>
+        <div className="card__content">
+          <h3>Resources</h3>
+          <p>
+            Majoring in STEM is a difficult task, so be sure to utilize
+              our  <a href="https://bergen.edu/wp-content/uploads/Running-Start-Program-Poster-With-Link-1.pdf" target="_blank" rel="noopener noreferrer">free prep classes</a> and
+            award-winning tutoring services to get the
+                            </p>
+        </div>
+      </div>
 
       <div className="clear">
-      <Link  to="/">Go back home</Link>    
-      </div>    
-    </div>    
+        <Link to="/">Go back home</Link>
+      </div>
+    </div>
   </Layout>
 )
 
