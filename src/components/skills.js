c61bd45196ab330347b04de2cf82c5ebfa9affb1
da crands <davecrands@gmail.com>
2019-03-24 11:43:31 -0400

    Added Video component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,70 +1,22 @@
 import React from 'react'
+import Video from './video'
 
 const Skills = () => (
 <section className="skills">
-
-
-{/* <header className="skills__title">
   <div className="container">
-    <h1>Skills</h1>
-  
+    <div className="box">
+      <div className="box__item">
+        <h2>Students talk STEM</h2>
+        <p>
+        Listen to BCC students discuss their
+            experiences in the STEM program.
+        </p>
+      </div>
+      <div className="box__video">
+        <Video />
+      </div>    
+    </div>    
   </div>
-</header> */}
-
-<div className="container">
-  <div className="skills__content">
-    <div className="skills__item">
-      <h2>Frontend</h2>
-      <p>
-        React
-      </p>
-      <p>
-        Redux/GraphQL
-      </p>
-      <p>Responsiveness via custom CSS
-  </p>
-  <p>
-  Semantic HTML  and other accessibility 
-  practices ensure quality user
-  experiences
-  </p>
-  <p>
-  Webpack and static-site generators like GatsbyJS
-  deliver fast page-loads
-  </p>
-      <p>
-        Digitalocean hosting using Nginx servers
-  </p>
-    </div>
-    <div className="skills__item">
-      <h2>Backend</h2>
-      <p>
-        Flask
-      </p>
-      <p>
-
-      </p>
-      <p>
-        Email support using Sendgrid
-      </p>
-      <p>
-        SQLAlchemy ORM to create databases
-      </p>
-      <p>
-        Bcrypt password encryption
-      </p>
-      <p>
-        RESTful routing
-      </p>
-      <p>
-        APIs implement CORS
-      </p>
-      <p>
-        Digitalocean hosting using Nginx servers
-      </p>
-    </div>
-  </div>
-</div>
 </section>
 )
 
