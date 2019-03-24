37922539d7cb8724ab6de356bd851273e7c06a9d
da crands <davecrands@gmail.com>
2019-03-24 11:41:06 -0400

    Updated links, smaller credit

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,17 @@ import React from 'react'
 const Footer = () => (
     <footer className="footer">
         <small className="footer__links">
-            <ul>
-                <li><a href="http://">Github</a></li>
-                <li><a href="http://">Blog</a></li>
-                <li><a href="http://">Resume</a></li>
-                <li><a href="http://">Contact</a></li>             
+            <ul>                
+                <li><a target="_blank" rel="noopener"  href="https://bergen.edu">bergen.edu</a></li>
+                <li><a target="_blank" rel="noopener" href="https://bergen.edu/faculty-staff/grants-administration/awards/stem/stem-summit/">
+            STEM Summit
+          </a></li>
+                <li><a rel="noopener" target="_blank" href="https://bergen.edu/wp-content/uploads/Running-Start-Program-Poster-With-Link-1.pdf">
+            Running Start Program</a></li>             
             </ul>   
-            <p>Designed and built by David Crandall</p>         
+            <small>
+                <p>Designed and built by David Crandall</p>            
+            </small>            
         </small>
     </footer>
 )
