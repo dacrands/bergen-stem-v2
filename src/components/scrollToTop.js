625f2c86e6f2ef901fba26d23334ced5a2b89a40
da crands <davecrands@gmail.com>
2019-03-28 21:54:52 -0400

    scrollTop btn sits on top of footer

--- src/components/scrollToTop.js
+++ src/components/scrollToTop.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react'
 
 class ScrollToTop extends Component {
   componentDidMount() {
-    window.addEventListener('scroll', () => {
-      window.scrollY > 300
-        ? (this.refs.scrollBtn.style.display = 'block')
-        : (this.refs.scrollBtn.style.display = 'none')
-    })
+    const footer = document.querySelector('.footer');
+    window.addEventListener('scroll', () => {      
+        if(this.refs.scrollBtn) {
+          window.scrollY > 300
+            ? (this.refs.scrollBtn.style.display = 'block')
+            : (this.refs.scrollBtn.style.display = 'none')
+
+            // Find intersection
+            const footerRect = footer.getBoundingClientRect();
+            const btnRect = this.refs.scrollBtn.getBoundingClientRect();
+
+            if ( btnRect.bottom >= footerRect.top ) {              
+              this.refs.scrollBtn.style.bottom = `${footer.clientHeight}px`;
+            } else {
+              this.refs.scrollBtn.style.bottom = `2rem`;
+            }
+            console.log(footer.clientHeight)
+
+
+        }        
+    })        
   }
   scrollTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
